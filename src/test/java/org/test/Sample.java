b5afcebd8e345cde68a7c3e44d825e876351384a
vinoth2636102 <vinothvenkatesan.913@gmail.com>
2022-09-07 00:26:06 -0700

    nivethitha's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -4,6 +4,10 @@ public class Sample {
 public static void main(String[] args) {
 	System.out.println("Test1");
 	System.out.println("Test2");
+	System.out.println("Test3");
+	System.out.println("Test4");
+	System.out.println("Test5");
+	System.out.println("Test6");
 }
 
 }
